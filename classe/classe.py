4c3694b918d45b27f234b58ec384eee47eda2c40
De Lafontaine <bruno.de-lafontaine.1@ens.etsmtl.ca>
2019-02-04 11:20:18 -0500

    add corefiles

---
+++ classe/classe.py
@@ -0,0 +1,34 @@
+class lien():
+
+	def __init__(self):
+		self.poids
+		self.source
+		self.destination
+		self.Func
+		self.poidsMod
+
+
+
+class neurone():
+
+	def __init__(self,valeur=1):
+		self.valeur=valeur
+		self.source
+		self.destination
+
+class couche():
+
+	def __init__(self):
+		self.tab_neurone #= neurone[]
+
+class reseaux():
+
+	def __init__(self):
+		self.tab_couche ="ye"#= couche[]	
+
+		
+
+
+
+
+		
