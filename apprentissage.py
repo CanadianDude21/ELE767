e790db0933599a3ef797fc6b982b544062e81c51
Fournier-Desrochers <mathieu.fournier-desrochers.1@ens.etsmtl.ca>
2019-02-12 13:02:10 -0500

    modif to creerReseau, added questions.txt

--- apprentissage.py
+++ apprentissage.py
@@ -1,30 +1,44 @@
 from classe import classe
 import random
 
-def creerReseau(nbCouches,nbNeurones,nbLiens):
-	
+def creerReseau(config):
+	nbCouches = int(config["nombreCoucheCacheEntree"])+2
+	nbNeuronesEntree = int(config["neuroneEntree"])
+	nbNeuronesCache = int(config["neuroneCacher"])
+	nbNeuronesSortie = int(config["neuroneSortie"])
+	nbDonneeEntrant = int(config["nbDonneeEntrant"])
+
+
 	bestReseau = classe.reseaux(nbCouches)
 	for i in range(nbCouches):
-		bestCouche = classe.couche(nbNeurones)
+		if i == 0:
+			bestCouche = classe.couche(nbNeuronesEntree)
+		elif i == 1:
+			bestCouche = classe.couche(nbNeuronesCache)
+		elif i == 2:
+			bestCouche = classe.couche(nbNeuronesSortie)
 		bestReseau.tabCouche.append(bestCouche)
 
-		for j in range(nbNeurones):
-			bestNeurone = classe.neurone(random.randrange(0,10),nbLiens) #TODO savoir quel poids mettre pour chaque neurone
+		for j in range(bestReseau.tabCouche[i].nbNeurones):
+			if i == 0:
+				bestNeurone = classe.neurone(random.randrange(0,10),nbDonneeEntrant) #TODO savoir quel poids mettre pour chaque neurone
+			else:
+				bestNeurone = classe.neurone(random.randrange(0,10),bestReseau.tabCouche[i-1].nbNeurones) #TODO savoir quel poids mettre pour chaque neurone
 			bestReseau.tabCouche[i].tabNeurone.append(bestNeurone)
 
-			for k in range(nbLiens):
+			for k in range(bestReseau.tabCouche[i-1].nbNeurones):
 				if i == 0:
 					bestLiens = classe.liens(random.uniform(-0.1, 0.1))
 				else:
-					bestLiens = classe.liens(random.uniform(-0.1, 0.1),k)
-				bestReseau.tabCouche[i].tabNeurone[j].tabLiens.append(bestNeurone)
+					bestLiens = classe.liens(random.uniform(-0.1, 0.1),bestReseau.tabCouche[i-1].tabNeurone[k])
+				bestReseau.tabCouche[i].tabNeurone[j].tabLiens.append(bestLiens)
 	return bestReseau
 
-def activation():
+#def activation():
 
-def sigErreur():
+#def sigErreur():
 
-def correction():
+#def correction():
 
-def actualisation():
+#def actualisation():
 
--- main.py
+++ main.py
@@ -3,10 +3,17 @@ import random
 import apprentissage as app
 from function import fetch
 
-nbCouches = 3
-nbNeurones = 3
-nbLiens = 3
 
-bestReseau = app.creerReseau(nbCouches,nbNeurones,nbLiens)
+config = fetch.getConfig()
+bestReseau = app.creerReseau(config)
 
+for couche in bestReseau.tabCouche:
+	print('nbNeurones = {}'.format(couche.nbNeurones))
+#	print(type(couche))
+	for neurone in couche.tabNeurone:
+		print('nbLiens = {}'.format(neurone.nbLiens))
+		for lien in neurone.tabLiens:
+			#print(type(lien))
+			print('poids = {}'.format(lien.poids))
+			print('source = {}'.format(lien.source))
 print("done")
