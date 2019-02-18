83cc2fd23706b80c02340757980680c16df2665f
Fournier-Desrochers <mathieu.fournier-desrochers.1@ens.etsmtl.ca>
2019-02-18 12:29:38 -0500

    start of activation and train methods

--- classe/classe.py
+++ classe/classe.py
@@ -3,15 +3,22 @@ import random
 
 class reseaux():
 
-	def __init__(self,config):
+	def __init__(self,configOriginal,donnee):
 
-		self.tauxApp = config["tauxApprentissage"]
-		self.lay0 = np.asarray(donnee)
-		self.lay1 = np.random.uniform(-0.1,0.1,(config["nbDonneeEntrant"], config["neuroneEntree"]))
-		self.lay2 = np.random.uniform(-0.1,0.1,(config["neuroneEntree"], config["neuroneCacher"]))
-		self.lay3 = np.random.uniform(-0.1,0.1,(config["neuroneCacher"], config["neuroneSortie"]))
+		self.config = configOriginal
+		self.tabEpoque = donnee
+		self.input = np.asarray(self.tabEpoque[random.randrange(0,len(self.tabEpoque))].data)
+		self.lay1 = np.random.uniform(-0.1,0.1,(self.config["nbDonneeEntrant"], self.config["neuroneEntree"]))
+		self.lay2 = np.random.uniform(-0.1,0.1,(self.config["neuroneEntree"], self.config["neuroneCacher"]))
+		self.lay3 = np.random.uniform(-0.1,0.1,(self.config["neuroneCacher"], self.config["neuroneSortie"]))
 
+	def activation(self,inputs,arrayPoids):
 
+		return self.config["delta"](np.dot(inputs,arrayPoids))
 
+	def train(self):
+		l1 = self.activation(self.input,self.lay1)
+		l2 = self.activation(l1,self.lay2)
+		l3 = self.activation(l2,self.lay3)
 
-		
+		l3_error = output_data - l3
