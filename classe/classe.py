d82beb77e3188d384ea2f916d176b2bafbc8147e
Fournier-Desrochers <mathieu.fournier-desrochers.1@ens.etsmtl.ca>
2019-02-26 12:14:52 -0500

    phase 2 faite

--- classe/classe.py
+++ classe/classe.py
@@ -20,10 +20,25 @@ class reseaux():
 
 		return self.config["delta"](np.dot(inputs,arrayPoids))
 
+	def correction(self,inputs,deltas):
+
+		return np.poids = self.config["tauxApprentissage"]
+
 	def train(self):
+		#Activation
 		l1 = self.activation(self.input,self.lay1)
 		l2 = self.activation(l1,self.lay2)
 		l3 = self.activation(l2,self.lay3)
 
+		#Signal d'erreur (Calcul des deltas d'erreur)
 		l3_error = self.output - l3
 		l3_delta = l3_error * self.config["delta"](l3,deriv=True)
+
+		l2_error = l3_delta.dot(self.lay3.T)
+		l2_delta = l2_error	* self.config["delta"](l2,deriv=True)
+
+		l1_error = l2_delta.dot(self.lay2.T)
+		l1_delta = l1_error	* self.config["delta"](l1,deriv=True)
+
+		#Correction (calcul deltas des poids)
+
