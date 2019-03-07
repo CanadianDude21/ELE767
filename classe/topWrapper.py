c44e75a1096d6a2a636db3e74c6f3e0ae8b2fe4c
Fournier-Desrochers <mathieu.fournier-desrochers.1@ens.etsmtl.ca>
2019-03-07 17:02:27 -0500

    completed all 3 main step of the algo

--- classe/topWrapper.py
+++ classe/topWrapper.py
@@ -37,8 +37,13 @@ class topWrapper():
 
 	# les foncitons ici sont des bouttons
 	def train(self):
-		algo.apprentissage(bestReseau,self.data)
+		algo.apprentissage(bestReseau,self.datasetTrain,self.output,nbrEpoques)
 
+	def VC(self):
+		algo.VC(bestReseau,datasetVC,output)
+
+	def generalisation(self):
+		algo.test(bestReseau,datasetTest,output)
 
 	def browse_dataset(self):
 		self.dataset_path_name = askopenfilename()
