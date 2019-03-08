477a7781a214507b930670ad97b2a50e80651a43
Fournier-Desrochers <mathieu.fournier-desrochers.1@ens.etsmtl.ca>
2019-03-08 16:44:53 -0500

    me rappelle pu

--- function/configPoids.py
+++ function/configPoids.py
@@ -25,45 +25,6 @@ def sauvegardePoids(reseau,path):
 				j+=1
 			#print("couche 1 done")
 		outfile.close()
-
-	# with open('config/configPoidsLayer2.txt', 'w') as outfile:
-	# 	data = reseau.lay2
-	# 	#print(reseau.lay2)
-	# 	outfile.write('# Poids couche 2: {0}\n#Neurone entrée 1\n'.format(data.shape))
-	# 	# Iterating through a ndimensional array produces slices along
-	# 	# the last axis. This is equivalent to data[i,:,:] in this case
-	# 	i = 2
-	# 	for data_slice in data:
-
-	# 	# The formatting string indicates that I'm writing out
-	# 	# the values in left-justified columns 7 characters in width
-	# 	# with 2 decimal places.  
-	# 		np.savetxt(outfile, data_slice, fmt='%-7.10f')
-
-	# 	# Writing out a break to indicate different slices...
-	# 		outfile.write('# Neurone entrée {}\n'.format(i))
-	# 		i+=1
-	# 	#print("couche 2 done")
-	# outfile.close()
-
-	# with open('config/configPoidsLayer3.txt', 'w') as outfile:
-	# 	data = reseau.lay3
-	# 	#print(reseau.lay3)
-	# 	outfile.write('# Poids couche 3: {0}\n#Neurone entrée 1\n'.format(data.shape))
-	# 	# Iterating through a ndimensional array produces slices along
-	# 	# the last axis. This is equivalent to data[i,:,:] in this case
-	# 	i = 2
-	# 	for data_slice in data:
-
-	# 	# The formatting string indicates that I'm writing out
-	# 	# the values in left-justified columns 7 characters in width
-	# 	# with 2 decimal places.  
-	# 		np.savetxt(outfile, data_slice, fmt='%-7.10f')
-
-	# 	# Writing out a break to indicate different slices...
-	# 		outfile.write('# Neurone entrée {}\n'.format(i))
-	# 		i+=1
-	# 	#print("couche 3 done")
 	print("fin de sauvegarde des poids")
 
 def chargerPoids(reseau,path):
@@ -72,13 +33,4 @@ def chargerPoids(reseau,path):
 	for i in range(reseau.config["nombreCoucheCachees"]+2):
 		new_data = np.loadtxt(path+"/configPoidsLayer"+str(i)+".txt")
 		reseau.lay[i] = new_data.reshape(reseau.lay[i].shape)
-	# new_data = np.loadtxt('config/configPoidsLayer1.txt')
-	# reseau.lay1 = new_data.reshape(reseau.lay1.shape)
-	# #print(reseau.lay1)
-	# new_data = np.loadtxt('config/configPoidsLayer2.txt')
-	# reseau.lay2 = new_data.reshape(reseau.lay2.shape)
-	# #print(reseau.lay2)
-	# new_data = np.loadtxt('config/configPoidsLayer3.txt')
-	# reseau.lay3 = new_data.reshape(reseau.lay3.shape)
-	# #print(reseau.lay3)
 	print("fin de chargement des poids")
--- main.py
+++ main.py
@@ -3,15 +3,30 @@ import sys
 sys.path.insert(0, "classe/")
 sys.path.insert(0, "function/")
 
-import classe, fetch, algo
+import classe, fetch, algo, configPoids
 import numpy as np
 
 config = fetch.getConfig("config/config.txt")
 configSortie = fetch.getConfigSortie(10)
-donne = fetch.getEpoque(40,"DATA/data_vc.txt")
-inputChoisie = np.asarray(donne[0].data)
+donneTrain = fetch.getEpoque(40,"DATA/data_train.txt")
+donneVC = fetch.getEpoque(40,"DATA/data_vc.txt")
+#inputChoisie = np.asarray(donne[0].data)
 output = np.asarray(configSortie)
-outputDesire = output[donne[0].resultat]
-bestReseau = classe.reseaux(config)
+#outputDesire = output[donne[0].resultat]
 
-algo.VC(bestReseau,donne,output)
+######test validation croisée
+reseau0Epoque = classe.reseaux(config)
+configPoids.chargerPoids(reseau0Epoque,"config/avant")
+reseau1Epoque = classe.reseaux(config)
+configPoids.chargerPoids(reseau1Epoque,"config/apres")
+print("reseau 0 époques")
+algo.VC(reseau0Epoque,donneVC,output)
+print("reseau 1 époques")
+algo.VC(reseau1Epoque,donneVC,output)
+
+###########test apprentissage
+# bestReseau = classe.reseaux(config)
+# configPoids.sauvegardePoids(bestReseau,"config/avant")
+# #configPoids.chargerPoids(bestReseau,"config/avant")
+# algo.apprentissage(bestReseau,donneTrain,output,1)
+# configPoids.sauvegardePoids(bestReseau,"config/apres")
