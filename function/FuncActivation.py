1123e26a82642a14310c48fe6be5d7c8e1d0e0b0
Fournier-Desrochers <mathieu.fournier-desrochers.1@ens.etsmtl.ca>
2019-03-14 14:00:21 -0400

    added relu function

--- function/FuncActivation.py
+++ function/FuncActivation.py
@@ -11,3 +11,11 @@ def tanh(x,deriv=False):
 		return np.tanh(x) #pas deriver
 	else:
 		return 1.0-np.tanh(x)**2 #deriver
+		
+def relu(x,deriv=False):
+	if deriv==False:
+		return np.maximum(x,0,x)
+	else:
+		x[x<=0] = 0
+		x[x>0] = 1
+		return x
