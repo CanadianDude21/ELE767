9b1ebbb87eb8fc57081c14a693e2c70e827396e3
CanadianDude21 <mathieufournier21@gmail.com>
2019-03-16 15:29:05 -0400

    added support of momentum in train method

--- classe/classe.py
+++ classe/classe.py
@@ -16,7 +16,10 @@ class reseaux():
 			self.lay.append(np.random.uniform(-0.1,0.1,(self.config["neuroneCacher"][i-1], self.config["neuroneCacher"][i])))
 			i+=1
 		self.lay.append(np.random.uniform(-0.1,0.1,(self.config["neuroneCacher"][i-1], self.config["neuroneSortie"])))
-		self.pourcentageTauxApp = 0.
+
+		self.momentum = 0.5
+		self.omegasDeltasPrec = []
+
 	def activation(self,inputs,arrayPoids):
 		return np.dot(inputs,arrayPoids)
 
@@ -24,48 +27,59 @@ class reseaux():
 		activations=[]
 		sortieFuncActivation = []
 		activations.append(self.activation(input,self.lay[0]))
-		sortieFuncActivation.append(self.config["fonctionActivation"](activations[0]))
+		sortieFuncActivation.append(self.config["foncActi"](activations[0]))
 		for i in range(self.config["nombreCoucheCachees"]+1):
 			activations.append(self.activation(sortieFuncActivation[i],self.lay[i+1]))
-			sortieFuncActivation.append(self.config["fonctionActivation"](activations[i+1]))
+			sortieFuncActivation.append(self.config["foncActi"](activations[i+1]))
 		
 		return sortieFuncActivation[-1] #output obtenue
 
-	def train(self, input, outputDesire,tauxApprVariable=False):
+	def train(self, input, outputDesire, momentum=False):
 		
 		#Activation
 		activations=[]
 		sortieFuncActivation = []
 		activations.append(self.activation(input,self.lay[0]))
-		sortieFuncActivation.append(self.config["fonctionActivation"](activations[0]))
+		sortieFuncActivation.append(self.config["foncActi"](activations[0]))
 		for i in range(self.config["nombreCoucheCachees"]+1):
 			activations.append(self.activation(sortieFuncActivation[i],self.lay[i+1]))
-			sortieFuncActivation.append(self.config["fonctionActivation"](activations[i+1]))
+			sortieFuncActivation.append(self.config["foncActi"](activations[i+1]))
 		
 		#Signal d'erreur (Calcul des deltas d'erreur)
 		if self.config["fonctionActivation"] == "sigmoid":
-	 		deltas=[]
-	 		deltas.insert(0,(outputDesire - sortieFuncActivation[-1])*self.config["fonctionActivation"](sortieFuncActivation[-1],deriv=True))
-	 		for i in range(self.config["nombreCoucheCachees"]+1):
-	 			deltas.insert(0,np.matmul(deltas[-1-i],self.lay[-1-i].T)*self.config["fonctionActivation"](sortieFuncActivation[-2-i],deriv=True)) 
+			deltas=[]
+			deltas.insert(0,(outputDesire - sortieFuncActivation[-1])*self.config["foncActi"](sortieFuncActivation[-1],deriv=True))
+			for i in range(self.config["nombreCoucheCachees"]+1):
+				deltas.insert(0,np.matmul(deltas[-1-i],self.lay[-1-i].T)*self.config["foncActi"](sortieFuncActivation[-2-i],deriv=True))
 		else:
 			deltas=[]
-			deltas.insert(0,(outputDesire - sortieFuncActivation[-1])*self.config["fonctionActivation"](activations[-1],deriv=True))
+			deltas.insert(0,(outputDesire - sortieFuncActivation[-1])*self.config["foncActi"](activations[-1],deriv=True))
 			for i in range(self.config["nombreCoucheCachees"]+1):
-				deltas.insert(0,np.matmul(deltas[-1-i],self.lay[-1-i].T)*self.config["fonctionActivation"](activations[-2-i],deriv=True)) 
+				deltas.insert(0,np.matmul(deltas[-1-i],self.lay[-1-i].T)*self.config["foncActi"](activations[-2-i],deriv=True))
 
-		#Correction taux apprentissage fixe
-		if tauxApprVariable is False:
-			omegasDeltas=[]
+		#Correction sans momentum
+		if momentum is False:
+			omegasDeltas = []
 			omegasDeltas.append(self.config["tauxApprentissage"]*np.outer(input, deltas[0]))
 			for i in range(self.config["nombreCoucheCachees"]+1):
 				omegasDeltas.append(self.config["tauxApprentissage"]*np.outer(sortieFuncActivation[i], deltas[i+1]))
-		#Correction taux apprentissage variable
+		#Correction avec momentum
 		else:
-			
-			
+			if len(self.omegasDeltasPrec) > 0:
+				omegasDeltas = []
+				omegasDeltas.append(self.config["tauxApprentissage"] * np.outer(input, deltas[0]))
+				for i in range(self.config["nombreCoucheCachees"] + 1):
+					omegasDeltas.append(self.config["tauxApprentissage"] * np.outer(sortieFuncActivation[i], deltas[i + 1]))
+				for i in range(len(omegasDeltas)):
+					omegasDeltas[i]+=self.momentum * self.omegasDeltasPrec[i]
+			else:
+				omegasDeltas = []
+				omegasDeltas.append(self.config["tauxApprentissage"] * np.outer(input, deltas[0]))
+				for i in range(self.config["nombreCoucheCachees"] + 1):
+					omegasDeltas.append(self.config["tauxApprentissage"] * np.outer(sortieFuncActivation[i], deltas[i + 1]))
 
 		#actualisation
 		for i in range(self.config["nombreCoucheCachees"]+1):
 			self.lay[i] += omegasDeltas[i]
+		self.omegasDeltasPrec = omegasDeltas
 		
