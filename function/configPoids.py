d93fa1789753cb2688db66dee724401dd446c599
Bruno <bdelafontaine92@gmail.com>
2019-03-16 21:35:28 -0400

    almost a final commit

--- function/configPoids.py
+++ function/configPoids.py
@@ -7,23 +7,12 @@ def sauvegardePoids(reseau,path):
 		data = reseau.lay[i]
 		# Write the array to disk
 		with open(path+"/configPoidsLayer"+str(i)+".txt", 'w') as outfile:
-		# I'm writing a header here just for the sake of readability
-		# Any line starting with "#" will be ignored by numpy.loadtxt
 			outfile.write('# Poids couche 1: {0}\n#Donnée 1\n'.format(data.shape))
-			# Iterating through a ndimensional array produces slices along
-			# the last axis. This is equivalent to data[i,:,:] in this case
 			j = 2
 			for data_slice in data:
-
-			# The formatting string indicates that I'm writing out
-			# the values in left-justified columns 7 characters in width
-			# with 2 decimal places.  
 				np.savetxt(outfile, data_slice, fmt='%-7.10f')
-
-			# Writing out a break to indicate different slices...
 				outfile.write('# Donnée {}\n'.format(j))
 				j+=1
-			#print("couche 1 done")
 		outfile.close()
 
 	print("fin de sauvegarde des poids")
