8dd015dc85cb759a0bb2341887e6ed34ed8f3479
De Lafontaine <bruno.de-lafontaine.1@ens.etsmtl.ca>
2019-03-18 11:12:33 -0400

    LVQ first ocmmit

---
+++ classe/lvq.py
@@ -0,0 +1,17 @@
+import numpy as np
+import random
+
+class lvq():
+
+	def __init__(self,Epoque,config):
+		#valeur des poids initialiser
+
+
+	def initPoids(self):
+	
+
+	def train(self):
+
+
+	def test(self,donnee):
+
