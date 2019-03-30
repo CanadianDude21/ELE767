bac59512c789bc0fc24c7f928f9b1de0906015e2
CanadianDude21 <mathieufournier21@gmail.com>
2019-03-30 18:56:19 -0400

    inutile

--- test.py
+++ test.py
@@ -13,25 +13,24 @@ donneTest = fetch.getEpoque(int(config["nbTrames"]),"DATA/data_test.txt")
 
 bestReseau = classe.lvq(donne,config,DVQ=True)
 
-# accVC=0
-# acc = 0
+accVC = 0
+acc = 0
+accTest = 0
 
 for x in range(len(donne)):
 	bestReseau.train(True)
 
 
-# for y in range(len(donneVC)):
-# 	#for y in range(len(donneVC[x].data)):
-# 		accVC += bestReseau.test(donneVC[y])
+for y in range(len(donneVC)):
+	accVC += bestReseau.test(donneVC[y])
 
 
-# for z in range(len(donneTest)):
-# 	#for y in range(len(donneVC[x].data)):
-# 		acc += bestReseau.test(donneTest[z])
-# print("VC")
-# print (accVC)
-# print (accVC/len(donneVC))
-# print("Test")
-# print (acc)
-# print (acc/len(donneTest))
+for z in range(len(donneTest)):
+	accTest += bestReseau.test(donneTest[z])
+print("VC")
+print(accVC)
+print (accVC/len(donneVC))
+print("Test")
+print (accTest)
+print (accTest/len(donneTest))
 
